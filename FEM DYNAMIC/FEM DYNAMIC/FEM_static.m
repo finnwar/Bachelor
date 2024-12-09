a96cd09ccf3d956fffbf279493464147ac898897
Finn Warlimont <finn.warlimont@tuhh.de>
2024-12-09 15:05:09 +0100

    Separate Node generation
    Removed the node generation from main and moved to separate function.

--- FEM DYNAMIC/FEM DYNAMIC/FEM_static.m
+++ FEM DYNAMIC/FEM DYNAMIC/FEM_static.m
@@ -24,8 +24,8 @@ H = 0.1;      %Höhe[m]
 forceDistribution = @(u,v) ([0;0]);
 
 %Auflösung
-NumberOfElementsX = 10; % Unterteilung der X-Richtung
-NumberOfElementsY = 3; % Unterteilung der Y-Richtung
+NumberOfElementsX = 100; % Unterteilung der X-Richtung
+NumberOfElementsY = 40; % Unterteilung der Y-Richtung
 TotalNumberOfElements = NumberOfElementsX*NumberOfElementsY;
 
 %% 
@@ -36,34 +36,7 @@ NumberOfNodesY = NumberOfElementsY+1;
 
 NumberOfNodes = (NumberOfElementsX+1)*(NumberOfElementsY+1);
 
-
-NodeGrid = zeros(2*NumberOfNodesX,NumberOfNodesY);
-
-%Numbering of global nodes
-for i=1:(2*NumberOfNodes)
-    NodeGrid(ind2sub(size(NodeGrid.'),i))=i;
-end
-%  Global nodes of elements
-%   Element progression:
-%  x _____________
-% y|  1,2   3,4   5,6   7,8
-%  |  9,10  11,12 13,14 15,16
-%  |  17,18 19,20 21,22 23,24
-NodeTable = zeros(NumberOfElementsX*NumberOfElementsY,8);
-
-for i = 1:(NumberOfElementsX)
-    for j = 1:(NumberOfElementsY)
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),1) = NodeGrid(2*i-1,j);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),2) = NodeGrid(2*i,j);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),3) = NodeGrid(2*i+1,j);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),4) = NodeGrid(2*i+2,j);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),5) = NodeGrid(2*i+1,j+1);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),6) = NodeGrid(2*i+2,j+1);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),7) = NodeGrid(2*i-1,j+1);
-        NodeTable(sub2ind([NumberOfElementsX,NumberOfElementsY],i,j),8) = NodeGrid(2*i,j+1);
-        
-    end
-end
+[NodeGrid, NodeTable] = NodeGridGenerator(NumberOfNodesX, NumberOfNodesY, NumberOfNodes, NumberOfElementsX, NumberOfElementsY);
 
 %Grid
 [x,y] = ndgrid(linspace(0,L,NumberOfNodesX),...
