1f0abd54c0ae36fc0908bb608b2cb5f41fa6a3c8
Finn Warlimont <finn.warlimont@tuhh.de>
2024-12-19 16:18:36 +0100

    Took out damping, changed some values

--- FEM DYNAMIC/FEM DYNAMIC/FEM_static.m
+++ FEM DYNAMIC/FEM DYNAMIC/FEM_static.m
@@ -25,8 +25,8 @@ H = 0.1;      %Höhe[m]
 forceDistribution = @(u,v) ([0;0]);
 
 %Auflösung
-NumberOfElementsX = 15; % Unterteilung der X-Richtung
-NumberOfElementsY = 5; % Unterteilung der Y-Richtung
+NumberOfElementsX = 100; % Unterteilung der X-Richtung
+NumberOfElementsY = 10; % Unterteilung der Y-Richtung
 TotalNumberOfElements = NumberOfElementsX*NumberOfElementsY;
 
 %% 
@@ -155,28 +155,34 @@ U_y = zeros(n/2,1);
 U_Gravity = K\(M*g*ones(size(f)));
 
 %Eigenmdoes
-[eigenVectors,eigenFrequencies] = eig(K,M);
+[eigenVectors,eigenFrequencies] = eig(K,M,'vector');
+
+[eigenFrequencies, index] = sort(eigenFrequencies);
+eigenVectors = eigenVectors(:,index);
+
 
 %Damping Matrix via Rayleigh-Coefficients
-alpha = 5;
-beta = 0.005;
+alpha = 0.005;
+beta = 5;
 
 D = alpha * M + beta * K;
 
 % Model Reduction
-Phi = [U,U_Gravity,eigenVectors(:,1:4)];
+Phi = [eigenVectors(:,1:10)];
 
 M_tilde = Phi'*M*Phi;
 K_tilde = Phi'*M*Phi;
 D_tilde = Phi'*D*Phi;
 f_tilde = Phi'*f;
 
+% The modal redcution produces under rank matrices when unfit modes are
+% chosen. Maybe not lin. ind. from eigenmodes?
 % Transient Analysis
 
-
+%%
 M_tilde_inv = inv(M_tilde);
 %opt = odeset('maxstep',1e-9);
-tspan = [0 1];
+tspan = [0 60];
 
 U_0 = U;
 q_0 = Phi\U_0;
@@ -184,15 +190,18 @@ q_0 = Phi\U_0;
 qdot_0 = zeros(length(q_0),1);
 qddot_0 = zeros(length(q_0),1);
 
+
+
 f_tilde_time = zeros(size(f_tilde));
 Y0 = [q_0;qdot_0];
 A = [zeros(size(K_tilde)) eye(size(K_tilde));
-     -M_tilde_inv*K_tilde -M_tilde_inv*D_tilde];
-b = [zeros(size(f_tilde_time));M_tilde_inv*f_tilde_time];
+     -M_tilde\K_tilde zeros(size(K_tilde))];            % Implementation of Damping Term leads to problems
+b = [zeros(size(f_tilde_time));M_tilde\f_tilde_time];
+%% 
 
-[t,Ysol] = ode45(@(t,Y) timeStepIntegration(t,Y,A,b), tspan, Y0);
+[t,Ysol] = ode23t(@(t,Y) timeStepIntegration(t,Y,A,b), tspan, Y0);
 
 
-U_sol = Phi * Ysol(:,1:length(Ysol(1,:))/2)';
+U_sol = Phi* Ysol(:,1:length(Ysol(1,:))/2)';
 figure
-plot(t,U_sol(end/4,:))
+plot(t,U_sol(end/4,:)')
