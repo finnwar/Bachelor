2b4e2624e493d0de1a8722c1588ac2e62db0f4d0
Finn Warlimont <finn.warlimont@tuhh.de>
2025-02-03 16:48:40 +0100

    Update PatchPlot.m

--- FEM DYNAMIC/Barbell/PatchPlot.m
+++ FEM DYNAMIC/Barbell/PatchPlot.m
@@ -40,11 +40,17 @@ for ii=1:length(t)
     %  determine displacement    
     
     verts = [tmp(1:2:end).'+1000*U(1:2:end,ii), tmp(2:2:end).'+ 1000*U(2:2:end,ii)];
-    
-    for e = 1:(NumberOfElementsY*NumberOfElementsX)
-        vertStress(NodeTable(e,2:2:8)/2) = nodeStress(e,:,ii).';
+    for y = 2:(NumberOfElementsY-1)
+        for x = 2:(NumberOfElementsX-1)
+            e = x+(y-1)*NumberOfElementsX;
+            vertStress(NodeTable(e,2:2:8)/2) = [0.25*(nodeStress(e,1,ii)+nodeStress(e-1-NumberOfElementsX,3,ii)+nodeStress(e-NumberOfElementsX,4,ii))+nodeStress(e-1,2,ii);
+                                                0.25*(nodeStress(e,2,ii)+nodeStress(e-NumberOfElementsX,3,ii)+nodeStress(e+1-NumberOfElementsX,4,ii))+nodeStress(e+1,1,ii);
+                                                0.25*(nodeStress(e,3,ii)+nodeStress(e+1,4,ii)+nodeStress(e+1+NumberOfElementsX,1,ii))+nodeStress(e+NumberOfElementsX,2,ii);
+                                                0.25*(nodeStress(e,4,ii)+nodeStress(e-1+NumberOfElementsX,2,ii)+nodeStress(e+NumberOfElementsX,1,ii))+nodeStress(e-1,3,ii);];
+        end
     end
-
+    vertStress(NodeTable(1:NumberOfElementsX,[2 4])/2) = [0.5*(nodeStress(2:NumberOfElementsX,1,ii)+nodeStress(1:(NumberOfElementsX-1),2,ii));
+                                                          0.5*(nodeStress(1:NumberOfElementsX,1,ii)+nodeStress(2:(NumberOfElementsX),2,ii))]
     % update vertice position
     set(pObj, 'vertices', verts, 'FaceVertexCData',vertStress);
 
