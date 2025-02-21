184585e4a76ebc95ac6218298e1c833b818329ee
Finn Warlimont <finn.warlimont@tuhh.de>
2025-02-21 17:14:38 +0100

    Simplified CMS
    Simplified the CMS function and the CMSTimeStepIntegration, reducing the number of precomputed matrices.

--- FEM DYNAMIC/Barbell/CMSTimeStepIntegration.m
+++ FEM DYNAMIC/Barbell/CMSTimeStepIntegration.m
@@ -1,13 +1,19 @@
-function dXdt = CMSTimeStepIntegration(t,A,X,invMiiPhiT,invMiiMie,invMiiDie,invMiiKie,NodeGrid)
+function dXdt = CMSTimeStepIntegration(t,A,X,invM_tilde_ii,M_tilde_ie,D_tilde_ie,K_tilde_ie,transV,NodeGrid)
     
     % Get Boundary conditions
     [U_e, BoundaryNodes, U_e_dot, U_e_ddot] = PositionBoundaryCondition(NodeGrid,t);
     [f,~] = ForceBoundaryCondition(NodeGrid,t);
+    f_e = f(BoundaryNodes);
+    f(BoundaryNodes)=[];
+    f_i = f;
+    f=[f_e;f_i];
+    f_tilde = transV*f;
+    f_tilde(1:length(BoundaryNodes))=[];
+    
     U_e=U_e(BoundaryNodes);
     U_e_dot=U_e_dot(BoundaryNodes);
     U_e_ddot=U_e_ddot(BoundaryNodes);
-    f(BoundaryNodes)=[];
-    % Reduce to eliminate bound degrees of freedom
-    f_hat= invMiiPhiT*f - invMiiMie * U_e_ddot - invMiiDie*U_e_dot - invMiiKie*U_e;
+    
+    f_hat = invM_tilde_ii*(f_tilde-M_tilde_ie*U_e_ddot-D_tilde_ie*U_e_dot-K_tilde_ie*U_e);
     dXdt = (A*X + [zeros(size(f_hat));f_hat]);
 end
--- FEM DYNAMIC/Barbell/DynamicCMSFEM.m
+++ FEM DYNAMIC/Barbell/DynamicCMSFEM.m
@@ -5,9 +5,9 @@ boundaryNodes = sort(boundaryNodes);
 if ~isempty(AdditionalModes)
     AdditionalModes(boundaryNodes,:)=[];
 end
-[invMiiPhiT,invMiiMie,invMiiDie,invMiiKie,invMiiDii,invMiiKii,invKiiKie,V_cms] = CMS(K,M,D,NumberOfModes, AdditionalModes, NodeGrid);
-
-
+[M_tilde_ii,D_tilde_ii,K_tilde_ii,M_tilde_ie,D_tilde_ie,K_tilde_ie,V_cms] = CMS(K,M,D,NumberOfModes, AdditionalModes, NodeGrid);
+invM_tilde_ii = inv(M_tilde_ii);
+transV = V_cms';
 q_0=V_cms\U_0;
 q_0(1:length(boundaryNodes))=[];
 q_dot_0=V_cms\U_dot_0;
@@ -16,12 +16,12 @@ q_dot_0(1:length(boundaryNodes))=[];
 
 X_0 = [q_0; q_dot_0];
 
-A = [zeros(size(invMiiKii)) eye(size(invMiiKii));
-     -invMiiKii -invMiiDii];
+A = [zeros(size(M_tilde_ii\K_tilde_ii)) eye(size(M_tilde_ii\K_tilde_ii));
+     -M_tilde_ii\K_tilde_ii -M_tilde_ii\D_tilde_ii];
 
 t_span = [0, 10];
 opt = odeset('MaxStep',1e-1);
-[t, X] = ode15s(@(t,X) CMSTimeStepIntegration(t,A,X,invMiiPhiT,invMiiMie,invMiiDie,invMiiKie,NodeGrid),t_span,X_0,opt);
+[t, X] = ode15s(@(t,X) CMSTimeStepIntegration(t,A,X,invM_tilde_ii,M_tilde_ie,D_tilde_ie,K_tilde_ie,transV,NodeGrid),t_span,X_0,opt);
 
 
 U_e = zeros(NodeGrid(end,end),length(t));
@@ -33,8 +33,8 @@ U_e = U_e(boundaryNodes,:);
 
 q_dyn = X(:,1:end/2).';
 
-
-U_i = -invKiiKie*U_e+Phi*q_dyn;
-
-U_dyn = BoundaryReinsertion(NodeGrid,t,boundaryNodes,U_i);
+q_dyn = [U_e; q_dyn]; 
+U_dyn = V_cms*q_dyn;
+U_dyn(1:length(boundaryNodes),:)=[];
+U_dyn = BoundaryReinsertion(NodeGrid,t,boundaryNodes,U_dyn);
 end
