bab565855dac71fc9f88ec7ddc09b2cc3ec141e2
Finn Warlimont <finn.warlimont@tuhh.de>
2025-03-04 21:07:38 +0100

    Update StressModeCalculation.m
    Stress modes for nodes, abscissas and center implemented. Not tested yet

--- FEM DYNAMIC/Barbell/StressModeCalculation.m
+++ FEM DYNAMIC/Barbell/StressModeCalculation.m
@@ -1,4 +1,4 @@
-function [Phi_vonMises,Phi_X,Phi_Y,Phi_XY] = StressModeCalculation(NodeGrid,NodeTable,NodePositionTable, nu, E)
+function [Phi_vonMises,Phi_X,Phi_Y,Phi_XY,Phi_Xnode,Phi_Ynode,Phi_XYnode,Phi_Xcenter,Phi_Ycenter,Phi_XYcenter] = StressModeCalculation(NodeGrid,NodeTable,NodePositionTable, nu, E)
     % Stress modes evaluated at abscissa and extrapolated to node
     Phi_vonMises = zeros(NodeGrid(end,end)/2,NodeGrid(end,end));
     
@@ -10,12 +10,14 @@ function [Phi_vonMises,Phi_X,Phi_Y,Phi_XY] = StressModeCalculation(NodeGrid,Node
     Phi_Ynode = zeros(NodeGrid(end,end)/2,NodeGrid(end,end));
     Phi_XYnode = zeros(NodeGrid(end,end)/2,NodeGrid(end,end));
     % Stress modes evaluated at the center
-    % Phi_Xcenter = zeros(NodeTable(:,1))
+    Phi_Xcenter = zeros(length(NodeTable(:,1)),NodeGrid(end,end));
+    Phi_Ycenter = zeros(length(NodeTable(:,1)),NodeGrid(end,end));
+    Phi_XYcenter = zeros(length(NodeTable(:,1)),NodeGrid(end,end));
     for i = 1:NodeGrid(end,end)
         unitLoad = zeros(NodeGrid(end,end),1);
         unitLoad(i) = 1;
     
-        [vonMises,~, sigmaX,sigmaY,tauXY,sigmaXnode,sigmaYnode,tauXYnode,~,~,~] = StressCalculation(unitLoad,1,nu,E,NodePositionTable,NodeTable);
+        [vonMises,~, sigmaX,sigmaY,tauXY,sigmaXnode,sigmaYnode,tauXYnode,sigmaXcenter,sigmaYcenter,tauXYcenter] = StressCalculation(unitLoad,1,nu,E,NodePositionTable,NodeTable);
         Phi_vonMises(:,i) = StressField(vonMises,NodeTable,NodeGrid);
         Phi_X(:,i) = StressField(sigmaX,NodeTable,NodeGrid);
         Phi_Y(:,i) = StressField(sigmaY,NodeTable,NodeGrid);
@@ -24,6 +26,10 @@ function [Phi_vonMises,Phi_X,Phi_Y,Phi_XY] = StressModeCalculation(NodeGrid,Node
         Phi_Xnode(:,i) = StressField(sigmaXnode,NodeTable,NodeGrid);
         Phi_Ynode(:,i) = StressField(sigmaYnode,NodeTable,NodeGrid);
         Phi_XYnode(:,i) = StressField(tauXYnode,NodeTable,NodeGrid);
+        
+        Phi_Xcenter(:,i) = sigmaXcenter;
+        Phi_Ycenter(:,i) = sigmaYcenter;
+        Phi_XYcenter(:,i) =tauXYcenter;
 
     end
 end
